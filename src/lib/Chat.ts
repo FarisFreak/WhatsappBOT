0ea6caaaa71d63cefcb256e9cc139f78bc6e9ac7
FarisFreak <faris.hasbir@gmail.com>
2023-07-04 20:30:13 +0700

    refactor

--- src/lib/Chat.ts
+++ src/lib/Chat.ts
@@ -3,72 +3,63 @@ import { proto } from '@whiskeysockets/baileys/WAProto/index.js';
 
 export class Chat {
     private _message : proto.IMessage | string = '';
-    private _prefix? : string;
-    private _command? : string;
-    private _value? : string;
-
     private _textMessage? : string;
 
-    constructor(msg: proto.IWebMessageInfo | string, prefix? : string) {
+    constructor(msg: proto.IWebMessageInfo | string, private prefix? : string) {
         if (typeof msg === 'string')
             this._message = msg as string;
         else 
             this._message = (msg as proto.IWebMessageInfo).message as proto.IMessage;
 
-        this._prefix = prefix ? prefix : config.prefix;
+        this.prefix = prefix ?? config.prefix;
 
         if (typeof this._message === 'string') {
             this._textMessage = this._message;
         } else {
-            if (this._message.extendedTextMessage)
-                this._textMessage = ((this._message as proto.IMessage).extendedTextMessage?.text as string);
-            else if (this._message.conversation)
-                this._textMessage = ((this._message as proto.IMessage).conversation as string);
-            else if (this._message.imageMessage?.caption)
-                this._textMessage = ((this._message as proto.IMessage).imageMessage?.caption as string);
-            else if (this._message.videoMessage?.caption)
-                this._textMessage = ((this._message as proto.IMessage).videoMessage?.caption as string);
-            else if (this._message.documentMessage?.caption)
-                this._textMessage = ((this._message as proto.IMessage).documentMessage?.caption as string);
+            const msg = this._message as proto.IMessage;
+
+            this._textMessage =
+                msg.extendedTextMessage?.text ??
+                msg.conversation ??
+                msg.imageMessage?.caption ??
+                msg.videoMessage?.caption ??
+                msg.documentMessage?.caption ??
+                '';
         }
 
     }
     public ContainsCommand (param: string) : boolean{
         let _message_split = this._textMessage?.split(' ');
         
-        if (_message_split?.find(x => x == `${this._prefix}${param}`))
+        if (_message_split?.find(x => x == `${this.prefix}${param}`))
             return true;
         
         return false;
     }
     public Get = {
-        Prefix : this._prefix,
-        Message : () => { return this._message; },
-        Command : () => {
-            let _message_split = this._textMessage?.split(' ');
+        Prefix : this.prefix,
+        Message : () => this._message,
+        Command : () : string => {
+            let _cmd : string = this._textMessage?.split(' ')[0] ?? '';
+            let output : string = _cmd[0] == this.prefix ? _cmd.replace(this.prefix, '') : '';
 
-            let _cmd: string = '';
-            if (_message_split)
-                _cmd = _message_split[0];
-
-            if (_cmd[0] == this._prefix)
-                this._command = _cmd.replace(this._prefix, '');
-
-            return this._command;
+            return output;
         },
         Value : (defaultValue?: string) => {
-            if (this.Get.Command() == null || this.Get.Command()?.length == 0 || this.Get.Command() == '')
+            let _value : string | undefined = '' as string;
+            
+            if (!this.Get.Command())
                 return null;
 
-            if (typeof this._message === 'string')
-                this._value = this._message.replace(this._prefix as string + this._command as string + ' ', '');
-            else
-                this._value = (this._message as proto.IMessage).conversation?.replace(this._prefix as string + this._command as string + ' ', '');
-            
-            if (this._value?.length == 0 || this._value == "" || this._value == null)
+            _value = typeof this._message === 'string' ?
+                this._message.replace(this.prefix as string + this.Get.Command() as string + ' ', '')
+                :
+                (this._message as proto.IMessage).conversation?.replace(this.prefix as string + this.Get.Command() as string + ' ', '');
+
+            if (!_value)
                 return defaultValue;
-            
-            return this._value;
+
+            return _value;
         }
     }
 }
